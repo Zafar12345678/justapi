99cf70813afa0b10946f65da1108fdc57578f715
Zafar12345678 <127655821+Zafar12345678@users.noreply.github.com>
2023-11-08 14:50:01 +0530

    update changes

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-
 const express = require("express");
-const app = express();
-const cors = require("cors");
+const http = require("http");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://merningday123:merningday123@cluster0.6llqgoh.mongodb.net/software?retryWrites=true&w=majority");
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(bodyParser.json());
+// Use cors middleware to enable CORS
 app.use(cors());
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function() {
-    console.log('Connected to MongoDB');
-});
-// const postRoutes = require("./routes/postRoutes");
-// app.use("/api", postRoutes);
-const post_route = require("./routes/postRoute")
-app.use("/api", post_route);
 
-app.listen(5000, function() {
-    console.log("Server is ready on port 5000");
-}); 
+// connection data base
+const PORT = 8000;
+const DB ="mongodb+srv://merningday123:merningday123@cluster0.6llqgoh.mongodb.net/software?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log(`Server is running on :${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
